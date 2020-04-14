4f389f795f38b69b0585b919a922568edbb3e46c
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-14 14:15:45 -0400

    added elements to Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 const Post = ({post}) => {
 
+  // the paramater is doing this:
+  // const {post} = props;
+
     return (  
         <div className={'box'}>
-        <h1>{post.posterName}</h1>
+        <h2>{post.title}</h2>
         <p>{post.resourceAuthor}</p>
-        <p>{post.title}</p>
+        {post.videoLength ? (<p>Length: {post.videoLength}</p>) : null }
+        {/* {conditional ? true code : false code */}
+        <p>Rating: {post.rating}</p>
+        <p>Comments: {post.comments.length}</p>
        <p>{post.length}</p>
         </div>
     )
   }
+
 
 
 export default Post
