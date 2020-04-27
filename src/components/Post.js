0e956ee3b15a82e36092d4957ad7b91163996e2b
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-04-27 03:39:25 -0400

    added state for true or false on click

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom'; // when clicked, the user will be redirected.
 
-const Post = ({post}) => {
+// Making Post.js a stateful component to be able to access the state so user gets redirected --
 
-  // the paramater is doing this:
-  // const {post} = props;
+class Post extends Component {
+  state = {
+    clicked: false
+  };
 
+  handleClick = () => {
+    this.props.handleSelecr(this.props.post.id); // This sends back the ID of the post.
+    this.SetState({clicked: true}) // Once an item is clicked, the state gets set to true.
+};
+
+render () {
+  const { post } = this.props;
     return (  
         <div className={'box'}>
         <h2 className="postTitle">{post.title}</h2>
@@ -17,7 +27,7 @@ const Post = ({post}) => {
         </div>
     )
   }
-
+}
 
 
 export default Post
