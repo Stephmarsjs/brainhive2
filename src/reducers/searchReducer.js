5c4a307395491b4b9f2b40280fe8eb43d157aa05
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-21 00:08:05 -0400

    added search_reducer

---
+++ src/reducers/searchReducer.js
@@ -0,0 +1,21 @@
+import {CHANGE_QUERY} from '../actions/types'
+
+const INITIAL_STORE = {
+    list: [],
+    query: "",
+    loading: false,
+    errors: {}
+}
+
+export default (store = INITIAL_STORE, action) => {
+    switch(action.type) {
+        case CHANGE_QUERY:
+            return {
+                ...store,
+                query: action.payload.query,
+                list: action.payload.list
+            }
+            default:
+                return store;
+    }
+}
