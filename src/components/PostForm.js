50fab24cb4fc5bba81ea0e674d57326cf306d0a4
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-05-21 01:25:36 -0400

    added post form map store to props

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,169 +1,158 @@
-import React, { Component } from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { changeForm, submitForm } from "../actions";
+import Button from "./common/Button";
 
-const INITIAL_STATE = {
-            id: 0,
-            posterName: "",
-            resourceAuthor: "",
-            jobSkillLevel: "",
-            cohort: "",
-            title: "",
-            categories: "",
-            summary: "",
-            link: "",
-            resroucetype: "",
-            datePublished: null,
-            videoLength: "",
-            timeToComplete: "",
-            rating: "",
-            comments: [],
-    }
+const PostForm = (props) => {
+  const { form } = props.newPost;
 
-   class PostForm extends Component {
-        state = { ...INITIAL_STATE };
-
-        handleChange = (e) => {
-          this.setState ({
-            ...this.state,
-            [e.target.id]: e.target.value,
-        });
-    };
-    handleSubmit = (e) => {
-      e.preventDefault();
-      const postData = { ...this.state };
-      postData.categories = postData.categories.split(",");
-      this.props.addPost(postData);
-      this.setState({ ...INITIAL_STATE });
-    };
-
-    render() {
-        return (
-          <div>
-            <form style={myStyles.form} onSubmit={(e) => this.handleSubmit(e)}>
-              <input
-                style={myStyles.input}
-                type="text"
-                id="posterName"
-                placeholder="Your Name"
-                value={this.state.posterName}
-                onChange={(e) => this.handleChange(e)}
-                required
-                 />
-              <input
-                style={myStyles.input}
-                type="text"
-                id="resourceAuthor"
-                placeholder="Author Name"
-                value={this.state.resourceAuthor}
-                onChange={(e) => this.handleChange(e)}
-                 />
-                 {/* dropdown skill level */}
-              <select
-                style={myStyles.input}
-                id="jobSkillLevel"
-                value={this.state.jobSkillLevel}
-                onChange={(e) => this.handleChange(e)}
-                  >
-                  <option value="" disabled>
-                    Author skill level
-                  </option>
-                  <option value="Intro">Intro</option>
-                  <option value="Intermediate">Intermediate</option>
-                  <option value="Advanced">Advanced</option>
-              </select>
-              <input
-                style={myStyles.input}
-                type="text"
-                id="cohort"
-                placeholder="Cohort #"
-                value={this.state.cohort}
-                onChange={(e) => this.handleChange(e)}
-                />
-               <input
-                style={myStyles.input}
-                type="text"
-                id="title"
-                placeholder="Title"
-                value={this.state.title}
-                onChange={(e) => this.handleChange(e)}
-                 />
-               <input
-                style={myStyles.input}
-                type="text"
-                id="categories" 
-                placeholder="Categories (separate multiples with commas)" 
-                value={this.state.categories}
-                onChange={(e) => this.handleChange(e)}
-                 />
-               <input
-                style={myStyles.input}
-                type="text"
-                id="link"
-                placeholder="Resource Link"
-                value={this.state.link}
-                onChange={(e) => this.handleChange(e)}  
-                 />
-               <input  
-                style={myStyles.input}
-                type="text"
-                id="resourceType"
-                placeholder="Resource Type"
-                value={this.state.resourceType}
-                onChange={(e) => this.handleChange(e)}
-                />
-              <input
-                style={myStyles.input}
-                type="date"
-                id="datePublished"
-                placeholder="Published Date"
-                value={this.state.datePublished}
-                onChange={(e) => this.handleChange(e)}
-                 />
-              <input
-                style={myStyles.input}
-                type="text"
-                id="videoLength"
-                placeholder="Length of video (optional)"
-                value={this.state.videoLength}
-                onChange={(e) => this.handleChange(e)}
-                />
-              <input
-                style={myStyles.input}
-                type="text"
-                id="timeToComplete"
-                placeholder="Time to complete resource"
-                value={this.state.timeToComplete}
-                onChange={(e) => this.handleChange(e)}
-              />
-              <input
-                style={myStyles.input}
-                type="text"
-                id="rating"
-                placeholder="1 to 5 rating"
-                value={this.state.rating}
-                onChange={(e) => this.handleChange(e)}
-              />
-              <button type="submit">Submit</button>
-           </form>
-        </div>
-      );
-    }
-  }
-  
-  const myStyles = {
-    form: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      marginRight: "15%",
-      marginLeft: "15%",
-      flexDirection: "column",
-    },
-    input: {
-      width: "80%",
-      height: 32,
-      fontSize: 20,
-      marginBottom: 4,
-    }
+  const handleChange = (e) => {
+    props.changeForm(e.target.id, e.target.value);
   };
 
-    export default PostForm;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // add any form validation
+    props.submitForm(props.newPost.form);
+  };
+
+  return (
+    <div>
+      <form style={myStyles.form} onSubmit={(e) => handleSubmit(e)}>
+        {/* <label htmlFor="posterName">Your Name: </label> */}
+        <input
+          style={myStyles.input}
+          type="text"
+          id="posterName"
+          placeholder="Your Name"
+          value={form.posterName}
+          onChange={(e) => handleChange(e)}
+          required
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="resourceAuthor"
+          placeholder="Author Name"
+          value={form.resourceAuthor}
+          onChange={(e) => handleChange(e)}
+        />
+        {/* dropdown skill level */}
+        <select
+          style={myStyles.input}
+          id="jobSkillLevel"
+          value={form.jobSkillLevel}
+          onChange={(e) => handleChange(e)}
+        >
+          <option value="" disabled>
+            Author skill level
+          </option>
+          <option value="Intro">Intro</option>
+          <option value="Intermediate">Intermediate</option>
+          <option value="Advanced">Advanced</option>
+        </select>
+        <input
+          style={myStyles.input}
+          type="text"
+          id="cohort"
+          placeholder="Cohort #"
+          value={form.cohort}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="title"
+          placeholder="title"
+          value={form.title}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="categories"
+          placeholder="Categories (seperate multiples with commas)"
+          value={form.categories}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="link"
+          placeholder="Resource Link"
+          value={form.link}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="resourceType"
+          placeholder="Resource Type"
+          value={form.resourceType}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="date"
+          id="datePublished"
+          placeholder="Published Date"
+          value={form.datePublished}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="videoLength"
+          placeholder="Length of Video (optional)"
+          value={form.videoLength}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="text"
+          id="timeToComplete"
+          placeholder="Time to complete resource"
+          value={form.timeToComplete}
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          style={myStyles.input}
+          type="number"
+          id="rating"
+          placeholder="1 to 5 rating"
+          value={form.rating}
+          onChange={(e) => handleChange(e)}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
+};
+
+const myStyles = {
+  form: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: "20%",
+    marginLeft: "20%",
+    flexDirection: "column",
+  },
+  input: {
+    width: "70%",
+    height: 32,
+    fontSize: 20,
+    marginBottom: 4,
+  },
+};
+
+const mapStoreToProps = (store) => {
+  return {
+    newPost: store.newPost,
+  };
+};
+
+export default connect(mapStoreToProps, {
+  changeForm,
+  submitForm,
+})(PostForm);
