8a07431880b8c1758e4387cbc1ba1b45340a8a81
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-06-08 22:51:24 -0400

    created user schema

--- Users.js
+++ Users.js
@@ -2,5 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
-    
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    lastLogin: Date
+}, {
+    timestamp: {}
 })
+
+module.exports = user = mongoose.model("users", userSchema);
