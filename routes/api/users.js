07d8fb1a2d2415fbebc21077c20ea212e350a261
Stephmarsjs <54284220+Stephmarsjs@users.noreply.github.com>
2020-06-09 23:20:04 -0400

    added routes and password hash

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,38 @@
 const express = require('express');
+const {check, validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
+const User = require('../../models/User')
 
 // @route POST api/users
 // @desc Create new user
 // @access Public
 
-router.post('/', (req,res) => {
-    res.json({body: req.body});
-});
+router.post(
+  '/',
+  [
+    check('email',  "Email is required").not().isEmpty(),
+    check('email', "Valid Email required").isEmail(),
+    check('name', "Name is required").not().isEmpty(),
+    check('password', "Password must contain at least 6 characters").isLength({min: 6})
+  ], 
+  async (req,res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({errors: errors.array()})
+      }
+      const userData ={
+          email: req.body.email,
+          password: req.body.password
+      }
+
+      const salt = await bcrypt.genSalt(10)
+      userData.password = await bcrypt.hash(userData.password, salt)
+
+      const user = await User.create(userData)
+      return res.json(user)
+  }
+);
 
 module.exports = router;
